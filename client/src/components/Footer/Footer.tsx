0bdc27197c9c3589f3c2dc697b6306e052583c2d
salimhs <74917442+salimhs@users.noreply.github.com>
2023-09-30 13:05:38 -0400

    Update Footer.tsx

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import "./Footer.style.scss";
 
 function Footer() {
-  return <div className="footer">Slam Dunk™ </div>;
+  return (
+    <div className="footer">
+      <p>© 2023 Carleton University</p>
+    </div>
+  );
 }
 
 export default Footer;
