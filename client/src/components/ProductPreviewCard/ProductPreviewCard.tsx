ca5f5c8b9b710cc93169afba40219482b0ce22fe
salimhs <74917442+salimhs@users.noreply.github.com>
2023-09-30 14:15:45 -0400

    Update ProductPreviewCard.tsx

--- client/src/components/ProductPreviewCard/ProductPreviewCard.tsx
+++ client/src/components/ProductPreviewCard/ProductPreviewCard.tsx
@@ -1,5 +1,7 @@
-import "./ProductPreviewCard.style.scss";
+import React from 'react';
+import './ProductPreviewCard.style.scss'; // Import styles
 
+// ProductPreviewCard component
 interface Props {
   title: string;
   description: string;
@@ -7,7 +9,7 @@ interface Props {
   imageUrl: string;
 }
 
-function ProductPreviewcard({ title, description, price, imageUrl }: Props) {
+function ProductPreviewCard({ title, description, price, imageUrl }: Props) {
   return (
     <div className="product-preview-card">
       <img className="product-preview-card__image" src={imageUrl} alt={title} />
@@ -18,4 +20,54 @@ function ProductPreviewcard({ title, description, price, imageUrl }: Props) {
   );
 }
 
-export default ProductPreviewcard;
+function App() {
+  // Define data for multiple product cards
+  const products = [
+    {
+      title: "Throwback Jerseys",
+      description: "Description for Product 1",
+      price: 19.99,
+      imageUrl: "https://cdn.vox-cdn.com/thumbor/uchBBEKqjFjdUCYrncfixLKJcMk=/0x0:2000x2000/1200x0/filters:focal(0x0:2000x2000):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/19279114/Lowry1.png",
+    },
+    {
+      title: "Product 2",
+      description: "Description for Product 2",
+      price: 24.99,
+      imageUrl: "https://example.com/product2.jpg",
+    },
+    {
+      title: "Product 3",
+      description: "Description for Product 3",
+      price: 29.99,
+      imageUrl: "https://example.com/product3.jpg",
+    },
+    {
+      title: "Product 4",
+      description: "Description for Product 4",
+      price: 14.99,
+      imageUrl: "https://example.com/product4.jpg",
+    },
+    {
+      title: "Product 5",
+      description: "Description for Product 5",
+      price: 34.99,
+      imageUrl: "https://example.com/product5.jpg",
+    },
+  ];
+
+  return (
+    <div>
+      {products.map((product, index) => (
+        <ProductPreviewCard
+          key={index}
+          title={product.title}
+          description={product.description}
+          price={product.price}
+          imageUrl={product.imageUrl}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
