7e43ac8ae9e01e578d965ff1e1ec73a4f1fcafaf
salimhs <74917442+salimhs@users.noreply.github.com>
2023-09-30 14:12:51 -0400

    Update ProductPreviewCard.tsx
    patch didnt wokr

--- client/src/components/ProductPreviewCard/ProductPreviewCard.tsx
+++ client/src/components/ProductPreviewCard/ProductPreviewCard.tsx
@@ -1,54 +1,21 @@
-import React from 'react';
-import ProductPreviewCard from './ProductPreviewCard'; // Adjust the import path as needed
+import "./ProductPreviewCard.style.scss";
 
-function App() {
-  // Define data for five different types of items
-  const items = [
-    {
-      title: "Throwback Jerseys",
-      description: "An assortment of throwback jerseys for a great price!!",
-      price: 19.99,
-      imageUrl: "https://cdn.vox-cdn.com/thumbor/uchBBEKqjFjdUCYrncfixLKJcMk=/0x0:2000x2000/1200x0/filters:focal(0x0:2000x2000):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/19279114/Lowry1.png",
-    },
-    {
-      title: "Item 2",
-      description: "Description for Item 2",
-      price: 24.99,
-      imageUrl: "https://example.com/item2.jpg",
-    },
-    {
-      title: "Item 3",
-      description: "Description for Item 3",
-      price: 29.99,
-      imageUrl: "https://example.com/item3.jpg",
-    },
-    {
-      title: "Item 4",
-      description: "Description for Item 4",
-      price: 14.99,
-      imageUrl: "https://example.com/item4.jpg",
-    },
-    {
-      title: "Item 5",
-      description: "Description for Item 5",
-      price: 34.99,
-      imageUrl: "https://example.com/item5.jpg",
-    },
-  ];
+interface Props {
+  title: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
 
+function ProductPreviewcard({ title, description, price, imageUrl }: Props) {
   return (
-    <div>
-      {items.map((item, index) => (
-        <ProductPreviewCard
-          key={index}
-          title={item.title}
-          description={item.description}
-          price={item.price}
-          imageUrl={item.imageUrl}
-        />
-      ))}
+    <div className="product-preview-card">
+      <img className="product-preview-card__image" src={imageUrl} alt={title} />
+      <h3 className="product-preview-card__title">{title}</h3>
+      <p className="product-preview-card__description">{description}</p>
+      <p className="product-preview-card__price">${price}</p>
     </div>
   );
 }
 
-export default App;
+export default ProductPreviewcard;
